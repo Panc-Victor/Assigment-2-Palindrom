d6b8202635549350b6240e701472b75210856ef4
Panc-Victor <panc_vctr@yahoo.com>
2021-05-23 19:32:55 +0300

    Palindrom

---
+++ Assigment-2-Palindrom/Assigment-2-Palindrom/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment_2_Palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string original = "AERISIREA";
+            string reversed = Reverse(original);
+            bool isPalindrome = IsPalindrom(original);
+
+            Console.WriteLine("Original=" + original);
+            Console.WriteLine("Reversed=" + reversed);
+            Console.WriteLine("IsPalindrome=" + isPalindrome);
+        }
+        private static bool IsPalindrom(string text)
+        {
+            string reversed = Reverse(text);
+            bool isPalyndrome = string.Equals(text, reversed, StringComparison.OrdinalIgnoreCase);
+            return isPalyndrome;
+        }
+    }
+}
